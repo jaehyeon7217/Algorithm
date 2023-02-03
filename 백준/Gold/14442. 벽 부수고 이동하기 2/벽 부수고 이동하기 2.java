7a6a50eab811b31e911b916436f0898fbbb22012
Park JaeHyeon <jhp1276@naver.com>
2023-02-03 10:16:28 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 2148 ms, Memory: 373900 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -0,0 +1,93 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static int N, M, K;
+	public static int[][] board;
+	public static boolean[][][] visit;
+	public static Queue<XY> q = new LinkedList<>();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		board = new int[N][M];
+		visit = new boolean[N][M][K+1];
+		
+		for(int i=0; i<N;i++) {
+			char[] temp = br.readLine().toCharArray();
+			for(int j=0;j<M;j++) {
+				board[i][j] = temp[j] - '0';
+			}
+		}
+		
+		bfs();
+		
+		br.close();
+		
+	}
+	
+	public static void bfs() {
+		q.offer(new XY(0,0,1,0));
+		visit[0][0][0] = true;
+		
+		while(!q.isEmpty()) {
+			XY temp = q.poll();
+		
+			if(temp.x == N-1 && temp.y == M-1) {
+				System.out.println(temp.count);
+				q.clear();
+				return;
+			}
+			
+			
+			int[] dx = {1,-1,0,0};
+			int[] dy = {0,0,1,-1};
+			
+			for(int i=0;i<4;i++) {
+				
+				int x = temp.x + dx[i];
+				int y = temp.y + dy[i];
+				
+				if(x>=0 && x<N && y>= 0 && y<M) {
+					if( visit[x][y][temp.wall] == false ) {
+						visit[x][y][temp.wall] = true;
+						
+						if(board[x][y] == 1 && temp.wall< K) {
+							q.offer(new XY(x,y,temp.count+1, temp.wall+1));
+						}
+						
+						if(board[x][y] == 0) {
+							q.offer(new XY(x,y,temp.count+1, temp.wall));
+						}
+						
+					}
+				}
+				
+			}
+			
+		}
+		
+		System.out.println("-1");
+	}
+	
+	
+	public static class XY{
+		int x;
+		int y;
+		int count;
+		int wall;
+		
+		XY(int x, int y, int count, int wall){
+			this.x = x;
+			this.y = y;
+			this.count = count;
+			this.wall = wall;
+		}
+	}
+}
