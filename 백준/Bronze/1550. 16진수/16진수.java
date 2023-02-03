f18121b66fd247d6d42fa2751e96711d30e5f2f7
Park JaeHyeon <jhp1276@naver.com>
2023-02-03 10:28:24 +0900

    [Bronze II] Title: 16진수, Time: 84 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		// ------------------------------------------------------------------------------
+		
+		int a= Integer.parseInt(br.readLine(),16);
+		
+			sb.append(a);
+		
+
+		// -----------------------------------------------------------------------------Main
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
+	
+		
+	}
+
+}
