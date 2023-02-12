a9b87e69f8108125bc777152657443345cde5c94
Park JaeHyeon <jhp1276@naver.com>
2023-02-12 19:27:49 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 576 ms, Memory: 77264 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int n,m;
+
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        TreeMap<String, String> map = new TreeMap<>();
+
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            map.put(st.nextToken(), st.nextToken());
+        }
+
+        for(int i=0;i<m;i++){
+            sb.append(map.get(br.readLine()) + "\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
