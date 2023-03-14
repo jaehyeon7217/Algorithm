17d68b858c5bb534592ece5435b4fcfeed38811c
Park JaeHyeon <jhp1276@naver.com>
2023-03-14 11:07:19 +0900

    [Bronze I] Title: XORXORXOR, Time: 84 ms, Memory: 11472 KB -BaekjoonHub

---
+++ 백준/Bronze/12833. XORXORXOR/XORXORXOR.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		if(c%2 == 0) System.out.println(a);
+		else System.out.println(a^b);
+		
+		br.close();
+	}
+}
