8b679d377cdced4b030fbbb6910fb6777acac45a
Park JaeHyeon <jhp1276@naver.com>
2023-05-08 12:57:40 +0900

    [Bronze V] Title: A+B, Time: 76 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,32 +1,14 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
-public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		// ------------------------------------------------------------------------------
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		
-		int a= Integer.parseInt(st.nextToken());
-		int b= Integer.parseInt(st.nextToken());
-		
-		sb.append(a+b);
-
-		// -----------------------------------------------------------------------------Main
-		bw.write(sb.toString());
-		bw.close();
-		br.close();
-	
-		
-	}
-
+import java.io.*;
+import java.util.*;
+public class Main{
+public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    
+    int 에이 = Integer.parseInt(st.nextToken());
+    int 비 = Integer.parseInt(st.nextToken());
+    
+    System.out.println(에이 + 비);
+    
+}
 }
