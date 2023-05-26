d94311374439e0099c956598b62f02d9099efdcc
Park JaeHyeon <jhp1276@naver.com>
2023-05-26 12:08:51 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 80 ms, Memory: 11624 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] map = new int[n];
+
+        for(int i=0;i<n;i++){
+            map[i] = i+1;
+        }
+
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken())-1;
+            int end = Integer.parseInt(st.nextToken())-1;
+
+            int size = (end-start+2)/2;
+
+            for(int j=start;j<start+size; j++){
+                int temp = map[j];
+                map[j] = map[end - (j-start)];
+                map[end - (j-start)] = temp;
+            }
+
+        }
+
+        for(int i=0;i<n;i++){
+            sb.append(map[i] + " ");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
