4b0d478288518ac7cd295c7f72ab037318d617b0
Park JaeHyeon <jhp1276@naver.com>
2023-06-13 16:16:52 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 580 ms, Memory: 52508 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -2,56 +2,57 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int n,m;
-    public static int[][] map;
-    public static int[][] result;
-    public static Queue<xy> q = new LinkedList<>();
-    public static int[] dx = {0,0,1,-1};
-    public static int[] dy = {1,-1,0,0};
+
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        Queue<xy> q = new LinkedList<>();
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
 
-        map = new int[n][m];
-        result = new int[n][m];
 
-        for(int i=0;i<n;i++){
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<m;j++){
+            for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                result[i][j] = -1;
-                if(map[i][j] == 2) {
-                    q.offer(new xy(i,j));
-                    result[i][j] = 0;
+                if(map[i][j] == 1) map[i][j] = -1;
+                else if (map[i][j] == 0) {
+                    map[i][j] = 0;
                 }
-                if(map[i][j] == 0) result[i][j] = 0;
+                else {
+                    q.offer(new xy(i, j));
+                    map[i][j] = 0;
+                }
 
             }
         }
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             xy temp = q.poll();
 
-            for(int i=0;i<4;i++){
-                int x = temp.x+dx[i];
-                int y = temp.y+dy[i];
+            for (int i = 0; i < 4; i++) {
+                int x = temp.x + dx[i];
+                int y = temp.y + dy[i];
 
-                if(x<0||x>=n||y<0||y>=m) continue;
-                if(result[x][y] != -1) continue;
+                if (x < 0 || x >= n || y < 0 || y >= m) continue;
+                if (map[x][y] != -1) continue;
 
-                result[x][y] = result[temp.x][temp.y] + 1;
-                q.offer(new xy(x,y));
+                map[x][y] = map[temp.x][temp.y] + 1;
+                q.offer(new xy(x, y));
 
             }
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                sb.append(result[i][j]).append(' ');
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                sb.append(map[i][j]).append(' ');
             }
             sb.append('\n');
         }
@@ -61,7 +62,7 @@ public class Main {
 
     }
 
-    public static class  xy{
+    public static class xy {
         int x;
         int y;
 
