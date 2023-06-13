a5d5a5454b3eddc9493657152bb333c831345c7d
Park JaeHyeon <jhp1276@naver.com>
2023-06-13 16:09:24 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 608 ms, Memory: 54432 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -5,7 +5,7 @@ public class Main {
     public static int n,m;
     public static int[][] map;
     public static int[][] result;
-    public static Queue<xy> q = new LinkedList<>();
+    public static Queue<int[]> q = new LinkedList<>();
     public static int[] dx = {0,0,1,-1};
     public static int[] dy = {1,-1,0,0};
     public static void main(String args[]) throws Exception {
@@ -25,7 +25,7 @@ public class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
                 result[i][j] = -1;
                 if(map[i][j] == 2) {
-                    q.offer(new xy(i,j));
+                    q.offer(new int[]{i,j});
                     result[i][j] = 0;
                 }
                 if(map[i][j] == 0) result[i][j] = 0;
@@ -34,42 +34,31 @@ public class Main {
         }
 
         while(!q.isEmpty()){
-            xy temp = q.poll();
+            int[] temp = q.poll();
 
             for(int i=0;i<4;i++){
-                int x = temp.x+dx[i];
-                int y = temp.y+dy[i];
+                int x = temp[0]+dx[i];
+                int y = temp[1]+dy[i];
 
                 if(x<0||x>=n||y<0||y>=m) continue;
-                if(map[x][y] != 1) continue;
                 if(result[x][y] != -1) continue;
 
-                result[x][y] = result[temp.x][temp.y] + 1;
-                q.offer(new xy(x,y));
+                result[x][y] = result[temp[0]][temp[1]] + 1;
+                q.offer(new int[]{x,y});
 
             }
         }
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                sb.append(result[i][j] + " ");
+                sb.append(result[i][j]).append(' ');
             }
-            sb.append("\n");
+            sb.append('\n');
         }
 
         System.out.println(sb);
 
 
-    }
-
-    public static class  xy{
-        int x;
-        int y;
-
-        public xy(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
     }
 
 }
