210695edea32f6918aaa38715b19dff8abdcda71
Park JaeHyeon <jhp1276@naver.com>
2024-02-20 15:56:26 +0900

    [Silver I] Title: 스티커, Time: 872 ms, Memory: 124132 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -26,20 +26,19 @@ public class Main {
             dp[0][0] = arr[0][0];
             dp[1][0] = arr[1][0];
 
-            if (N == 1) {
-                result = Math.max(dp[0][0], dp[1][0]);
-            } else {
-
+            if (N > 1) {
                 dp[0][1] = dp[1][0] + arr[0][1];
                 dp[1][1] = dp[0][0] + arr[1][1];
-
-                for (int i = 2; i < N; i++) {
-                    dp[0][i] = Math.max(dp[1][i - 2], dp[1][i - 1]) + arr[0][i];
-
-                    dp[1][i] = Math.max(dp[0][i - 2], dp[0][i - 1]) + arr[1][i];
-                }
-                result = Math.max(dp[1][N - 1], dp[0][N - 1]);
             }
+
+
+            for (int i = 2; i < N; i++) {
+                dp[0][i] = Math.max(dp[1][i - 2], dp[1][i - 1]) + arr[0][i];
+
+                dp[1][i] = Math.max(dp[0][i - 2], dp[0][i - 1]) + arr[1][i];
+            }
+            result = Math.max(dp[1][N - 1], dp[0][N - 1]);
+            
 
             sb.append(result + "\n");
         }
