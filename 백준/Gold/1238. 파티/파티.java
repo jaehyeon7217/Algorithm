2a12d2863b405d44875dd71314b228469cd7322b
Park JaeHyeon <jhp1276@naver.com>
2024-02-21 11:57:00 +0900

    [Gold III] Title: 파티, Time: 156 ms, Memory: 16460 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -3,16 +3,33 @@ import java.util.*;
 
 public class Main {
 
+    public static ArrayList<ArrayList<XY>> go, back;
+    public static int[] goCheck, backCheck;
+    public static int N,M,X;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken())-1;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken())-1;
 
-        int[][] road = new int[N][N];
+        go = new ArrayList<>();
+        back = new ArrayList<>();
+
+        goCheck = new int[N];
+        backCheck = new int[N];
+
+        for(int i=0;i<N;i++){
+            goCheck[i] = Integer.MAX_VALUE;
+            backCheck[i] = Integer.MAX_VALUE;
+        }
+
+        for(int i=0;i<N;i++){
+            go.add(new ArrayList<>());
+            back.add(new ArrayList<>());
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
@@ -20,81 +37,47 @@ public class Main {
             int end = Integer.parseInt(st.nextToken())-1;
             int cost = Integer.parseInt(st.nextToken());
 
-            road[start][end] = cost;
+            go.get(start).add(new XY(end, cost));
+            back.get(end).add(new XY(start, cost));
+
         }
+
+        func(go, goCheck);
+        func(back, backCheck);
+
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < N; i++) {
-            if (i == X) continue;
-
-            PriorityQueue<XY> list = new PriorityQueue<>();
-
-            list.add(new XY(i, 0));
-
-            int min = Integer.MAX_VALUE;
-            int[] check = new int[N];
-
-            for (int j = 0; j < N; j++) {
-                check[j] = Integer.MAX_VALUE;
-            }
-            check[i] = 0;
-
-            while (!list.isEmpty()) {
-                XY temp = list.poll();
-
-                if (temp.x == X) {
-                    min = Math.min(min, temp.cost);
-//                    System.out.println("가는 길 temp.x = "+temp.x + ", temp.cost = " + temp.cost);
-                    break;
-                }
-
-                for (int j = 0; j < N; j++) {
-                    if (road[temp.x][j] != 0 && (check[j] > temp.cost + road[temp.x][j])) {
-                        check[j] = temp.cost + road[temp.x][j];
-                        list.offer(new XY(j, temp.cost + road[temp.x][j]));
-                    }
-                }
-
-            }
-            int sum = min;
-
-            list = new PriorityQueue<>();
-
-            list.add(new XY(X, 0));
-
-            min = Integer.MAX_VALUE;
-            check = new int[N];
-
-            for (int j = 0; j < N; j++) {
-                check[j] = Integer.MAX_VALUE;
-            }
-            check[X] = 0;
-
-            while (!list.isEmpty()) {
-                XY temp = list.poll();
-
-                if (temp.x == i) {
-                    min = Math.min(min, temp.cost);
-//                    System.out.println("오는 길 temp.x = "+temp.x + ", temp.cost = " + temp.cost);
-                    break;
-                }
-
-                for (int j = 0; j < N; j++) {
-                    if (road[temp.x][j] != 0 && (check[j] > temp.cost + road[temp.x][j])) {
-                        check[j] = temp.cost + road[temp.x][j];
-                        list.offer(new XY(j, temp.cost + road[temp.x][j]));
-                    }
-                }
-
-            }
-            sum += min;
-            max = Math.max(max, sum);
-
+        for(int i=0;i<N;i++){
+            max = Math.max(max, goCheck[i] + backCheck[i]);
         }
 
         System.out.println(max);
-
-
     }
+
+    public static void func(ArrayList<ArrayList<XY>> arr, int[] check){
+        for(int i=0;i<N;i++){
+            check[i] = Integer.MAX_VALUE;
+        }
+
+        PriorityQueue<XY> list = new PriorityQueue<>();
+        list.add(new XY(X, 0));
+        check[X] = 0;
+
+        while(!list.isEmpty()){
+            XY temp = list.poll();
+
+
+            for(int i=0;i<arr.get(temp.x).size(); i++){
+                XY getTemp = arr.get(temp.x).get(i);
+
+                if(check[getTemp.x] > getTemp.cost + temp.cost){
+                    check[getTemp.x] = getTemp.cost + temp.cost;
+                    list.add(new XY(getTemp.x, getTemp.cost + temp.cost));
+                }
+
+            }
+        }
+    }
+
 
     public static class XY implements Comparable<XY> {
         int x;
