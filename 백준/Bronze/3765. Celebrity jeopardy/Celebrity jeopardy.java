6345a474164b0ee800cf7388bd7aafc63a835090
Park JaeHyeon <jhp1276@naver.com>
2024-03-31 10:07:01 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 72 ms, Memory: 11284 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            String s = br.readLine();
+
+            if(s == null)break;
+            System.out.println(s);
+        }
+
+
+    }
+
+
+}
