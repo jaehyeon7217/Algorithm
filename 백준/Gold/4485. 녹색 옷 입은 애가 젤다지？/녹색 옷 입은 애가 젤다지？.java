918ec653677ac8cd1484d47fd41352adf49f2492
Park JaeHyeon <jhp1276@naver.com>
2024-04-08 10:27:56 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 164 ms, Memory: 16816 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -5,7 +5,8 @@ public class Main {
 
     public static int n;
     public static final int max = 1234567890;
-    public static int[][] map, check;
+    public static int[][] map;
+    public static boolean[][] check;
     public static int[] dx = {0,0,1,-1}, dy = {1,-1,0,0};
 
     public static void main(String[] args) throws Exception {
@@ -21,36 +22,41 @@ public class Main {
             if(n == 0) break;
 
             map = new int[n][n];
-            check = new int[n][n];
+            check = new boolean[n][n];
 
             for(int i=0;i<n;i++){
                 st = new StringTokenizer(br.readLine());
                 for(int j=0;j<n;j++){
                     map[i][j] = Integer.parseInt(st.nextToken());
-                    check[i][j] = max;
                 }
             }
 
-            Queue<node> q = new LinkedList<>();
-            q.offer(new node(0,0));
-            check[0][0] = map[0][0];
+            PriorityQueue<node> q = new PriorityQueue<>();
+            q.offer(new node(0,0, map[0][0]));
+            check[0][0] = true;
+
+            int result = 0;
 
             while(!q.isEmpty()){
                 node temp = q.poll();
+
+                if(temp.x == n-1 && temp.y == n-1){
+                    result = temp.value;
+                    break;
+                }
 
                 for(int i=0;i<4;i++){
                     int x = temp.x + dx[i];
                     int y = temp.y + dy[i];
 
                     if(x <0 || x>=n || y<0 || y>=n) continue;
-                    if(check[x][y] <= check[temp.x][temp.y] + map[x][y]) continue;
-
-                    check[x][y] = check[temp.x][temp.y] + map[x][y];
-                    q.offer(new node(x,y));
+                    if(check[x][y]) continue;
+                    check[x][y] = true;
+                    q.offer(new node(x,y, temp.value + map[x][y]));
                 }
             }
 
-            sb.append("Problem ").append(t).append(": ").append(check[n-1][n-1]).append("\n");
+            sb.append("Problem ").append(t).append(": ").append(result).append("\n");
             t++;
 
         }
@@ -58,12 +64,19 @@ public class Main {
         System.out.println(sb.toString());
     }
 
-    public static class node{
+    public static class node implements Comparable<node>{
         int x;
         int y;
-        public node(int x, int y){
+        int value;
+        public node(int x, int y, int value){
             this.x = x;
             this.y = y;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(node o){
+            return this.value - o.value;
         }
     }
 
