381048ba4798cf528d47a6721917f5e33c73591d
Park JaeHyeon <jhp1276@naver.com>
2024-04-16 21:22:07 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6560 ms, Memory: 372128 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger one = new BigInteger(st.nextToken());
+        BigInteger two = new BigInteger(st.nextToken());
+
+        System.out.println(one.multiply(two));
+
+    }
+}
