0d06cb4a19a5773e62bdd969fe6bb23ce7da789c
Park JaeHyeon <jhp1276@naver.com>
2024-04-23 07:56:20 +0900

    [Bronze IV] Title: 타임 카드, Time: 72 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Bronze/5575. 타임 카드/타임 카드.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+
+public class Main {
+    public static StringBuilder sb = new StringBuilder();
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static void main(String[] args) throws Exception {
+
+            func();
+            func();
+            func();
+
+        System.out.println(sb);
+
+    }
+
+    public static void func() throws Exception {
+        int startH, startM, startS, endH, endM, endS, resultH, resultM;
+
+        st = new StringTokenizer(br.readLine());
+        startH = Integer.parseInt(st.nextToken());
+        startM = Integer.parseInt(st.nextToken());
+        startS = Integer.parseInt(st.nextToken());
+        endH = Integer.parseInt(st.nextToken());
+        endM = Integer.parseInt(st.nextToken());
+        endS = Integer.parseInt(st.nextToken());
+
+        int start, end, result;
+
+        start = (startH * 3600) + (startM * 60) + startS;
+        end = (endH* 3600 ) + (endM * 60) + endS;
+        result = end-start;
+        resultH = result/3600;
+        result = result - (resultH*3600);
+        resultM = result/60;
+        result = result - (resultM*60);
+
+        sb.append(resultH + " " + resultM + " " +result + "\n");
+
+    }
+
+
+}
