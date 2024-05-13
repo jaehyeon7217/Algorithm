0d1af34c9942b8df68c70d3d669a11ab7298bcd2
Park JaeHyeon <jhp1276@naver.com>
2024-05-14 02:06:58 +0900

    [Gold III] Title: 할로윈의 양아치, Time: 608 ms, Memory: 105328 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.java
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.java
@@ -3,113 +3,83 @@ import java.util.*;
 
 public class Main {
 
-    public static int n,m,k;
-    public static int[] kids, friend;
-
+    public static int n, m, k;
+    public static int[] child, arr, countChild, countCost;
+    public static int[] result;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        // n은 거리에 있는 아이들의 수,
-        // m은 아이들의 친구 관계 수,
-        // k는 울음소리가 공명하기 위한 최소 아이의 수이다.
         n = Integer.parseInt(st.nextToken());
-        m= Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        // 아이들이 받은 사탕의 수
-        kids = new int[n];
+        child = new int[n];
+        arr = new int[n];
+        countChild = new int[n];
+        countCost = new int[n];
+        result = new int[k];
 
-        st = new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++){
-            kids[i] = Integer.parseInt(st.nextToken());
+            arr[i] = i;
         }
 
-        // 친구관계 받기
-
-            // Union-Find 알고리즘을 위한 셋팅
-        friend = new int[n];
+        st = new StringTokenizer(br.readLine(), " ");
         for(int i=0;i<n;i++){
-            friend[i] = i;
+            child[i] = Integer.parseInt(st.nextToken());
         }
-            // Union 적용
-            // 무리를 지음
+
         for(int i=0;i<m;i++){
-            st = new StringTokenizer(br.readLine());
-
-            int one = Integer.parseInt(st.nextToken())-1;
-            int two = Integer.parseInt(st.nextToken())-1;
-
-            union(one, two);
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken())-1;
+            int b = Integer.parseInt(st.nextToken())-1;
+            union(a,b);
         }
 
-
-        // 무리에 가입된 아이의 수와 그 무리의 사탕 수
-        int[] countKids = new int[n];
-        int[] sumKids = new int[n];
         for(int i=0;i<n;i++){
             find(i);
-
-            countKids[friend[i]]++;
-            sumKids[friend[i]] += kids[i];
+            countChild[arr[i]]++;
+            countCost[arr[i]] += child[i];
         }
 
-
-        // 무리의 수가 0이 아니라면 한 무리
-        // 무리수와 사탕수를 추출
-        LinkedList<node> list = new LinkedList<>();
         for(int i=0;i<n;i++){
-            if(countKids[i] == 0) continue;
-            list.add(new node(countKids[i], sumKids[i]));
-        }
+            if(countChild[i] == 0) continue;
+            if(countChild[i]>=k) continue;
+            boolean[] check = new boolean[k];
 
-        // Knapsack
-        int size = list.size();
-        int[][] dp = new int[size+1][k];
-        for(int i=1;i<size+1;i++){
-            node temp = list.poll();
-            for(int j=0;j<k;j++){
-                if(j<temp.count){
-                    dp[i][j] =dp[i-1][j];
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-temp.count] + temp.cost);
+            for(int j=k-1; j>=countChild[i]; j-- ){
+                if(check[j-countChild[i]]) continue;
+                if(result[j] < result[j-countChild[i]]+countCost[i]){
+                    check[j] = true;
+                    result[j] = result[j-countChild[i]] + countCost[i];
                 }
             }
+
         }
 
-
-        System.out.println(dp[size][k-1]);
+        System.out.println(result[k-1]);
 
     }
-
     public static void union(int a, int b){
         a = find(a);
         b = find(b);
 
         if(a<b){
-            friend[b] = a;
+            arr[b] = a;
         }else{
-            friend[a] = b;
+            arr[a] = b;
         }
     }
+
     public static int find(int a){
-        if(friend[a] == a){
+        if(arr[a] == a){
             return a;
-        }
-
-        return friend[a] = find(friend[a]);
-    }
-
-    public static class node{
-        int count;
-        int cost;
-
-        public node(int count, int cost){
-            this.count = count;
-            this.cost = cost;
+        } else{
+            return arr[a] = find(arr[a]);
         }
     }
+
 
 
 }
