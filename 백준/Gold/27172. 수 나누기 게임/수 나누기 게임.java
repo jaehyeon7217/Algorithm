03a2870eba56bbeed3d16d0ddabcb217b67b04e3
Park JaeHyeon <jhp1276@naver.com>
2024-05-14 13:52:56 +0900

    [Gold V] Title: 수 나누기 게임, Time: 376 ms, Memory: 48788 KB -BaekjoonHub

---
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int n;
+    public static int[] arr, result;
+    public static final int max = 1_000_001;
+    public static boolean[] all = new boolean[max];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n];
+        result = new int[max];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            all[arr[i]]=true;
+        }
+
+        for(int i=0;i<max;i++){
+            if(!all[i]) continue;
+            for(int j=i+i;j<max;j+=i){
+                if(!all[j]) continue;
+
+                result[i]++;
+                result[j]--;
+
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n;i++){
+            sb.append(result[arr[i]] + " ");
+        }
+        System.out.println(sb);
+
+
+    }
+
+
+}
