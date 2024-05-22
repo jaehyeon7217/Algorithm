9f0d5cac4c71ca1c7632a39cc9c763b97cb9dd91
Park JaeHyeon <jhp1276@naver.com>
2024-05-22 22:36:17 +0900

    [Bronze V] Title: 직사각형, Time: 76 ms, Memory: 11404 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        System.out.println(x*y);
+
+    }
+
+
+}
