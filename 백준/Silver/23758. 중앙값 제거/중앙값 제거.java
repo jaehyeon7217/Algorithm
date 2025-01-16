bda30f7e30db559bfeddef3457dc87d9e7dd7ea3
Park JaeHyeon <jhp1276@naver.com>
2025-01-17 07:17:33 +0900

    [Silver I] Title: 중앙값 제거, Time: 640 ms, Memory: 344600 KB -BaekjoonHub

--- 백준/Silver/23758. 중앙값 제거/중앙값 제거.java
+++ 백준/Silver/23758. 중앙값 제거/중앙값 제거.java
@@ -4,45 +4,63 @@ import java.util.*;
 
 public class Main {
 
+    public static int[] compareNum = new int[31];
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        compareNum[1] = 2;
+
+        for (int i = 2; i <= 30; i++) {
+            compareNum[i] = compareNum[i - 1] * 2;
+        }
+
         int n = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine(), " ");
 
+        int[] arr = new int[30];
+
         int[] num = new int[n];
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             num[i] = Integer.parseInt(st.nextToken());
+
+            arr[count(num[i])]++;
+
         }
 
-        Arrays.sort(num);
+        int center = (n + 1) / 2;
+        int countCheck = 0;
+        int sum = 0;
 
-        int center = (n+1)/2;
-
-        int count = 0;
-
-        for(int i=0;i<center;i++){
-            count+= count(num[i]);
+        for (int i = 0; i < 30; i++) {
+            if(countCheck + arr[i] > center){
+                int temp = center - countCheck;
+                sum += i*temp;
+                break;
+            }
+            countCheck+=arr[i];
+            sum += i*arr[i];
+            if(countCheck == center) break;
         }
 
-        System.out.println(count+1);
-
+        System.out.println(sum +1);
 
 
     }
 
-    public static int count(int num){
-        int count = 0;
-
-        while(num != 1){
-            num/=2;
-            count++;
+    public static int count(int num) {
+        int ans = 0;
+        for (int i = 1; i < 31; i++) {
+            if (compareNum[i] > num) {
+                ans = i - 1;
+                break;
+            }
         }
-        return count;
+        return ans;
+
     }
 
 
