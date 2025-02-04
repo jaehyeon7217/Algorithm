888a629d15320f68a9b27f5cb43da2441f81ab4a
Park JaeHyeon <jhp1276@naver.com>
2025-02-04 22:41:37 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 64 ms, Memory: 11724 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -10,29 +10,19 @@ public class Main {
 
         int T = Integer.parseInt(br.readLine());
 
-        for(int t = 0;t<T;t++){
+        for (int t = 0; t < T; t++) {
             int c = Integer.parseInt(br.readLine());
 
-            int q=0,d=0,n=0,p=0;
+            int q = c / 25;
+            c %= 25;
+            int d = c / 10;
+            c %= 10;
+            int n = c/5;
+            c %= 5;
+            int p = c;
 
-            while(c>=25){
-                c-=25;
-                q++;
-            }
-            while(c>=10){
-                c-=10;
-                d++;
-            }
-            while(c>=5){
-                c-=5;
-                n++;
-            }
-            while(c>=1){
-                c-=1;
-                p++;
-            }
 
-            sb.append(q + " " + d+ " " +n+ " " +p+"\n");
+            sb.append(q + " " + d + " " + n + " " + p + "\n");
 
         }
 
