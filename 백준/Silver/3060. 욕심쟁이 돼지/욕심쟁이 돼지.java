5417b8d06ca2bffef84523ac45a94dc1697555f6
Park JaeHyeon <jhp1276@naver.com>
2025-02-12 20:49:51 +0900

    [Silver V] Title: 욕심쟁이 돼지, Time: 68 ms, Memory: 11820 KB -BaekjoonHub

--- 백준/Silver/3060. 욕심쟁이 돼지/욕심쟁이 돼지.java
+++ 백준/Silver/3060. 욕심쟁이 돼지/욕심쟁이 돼지.java
@@ -13,33 +13,16 @@ public class Main {
         for(int t=0;t<T;t++){
             int n = Integer.parseInt(br.readLine());
 
-            long[] arr = new long[6];
+            int sum=0;
 
             st = new StringTokenizer(br.readLine());
             for(int i=0;i<6;i++){
-                arr[i] = Long.parseLong(st.nextToken());
+                sum += Integer.parseInt(st.nextToken());
             }
             int count=1;
-            while(true){
-                long sum= 0;
-
-                for(int i=0;i<6;i++){
-                    sum+=arr[i];
-                }
-
-                if(sum <=n ){
-                    count++;
-                    long[] newArr = new long[6];
-                    for(int i=0;i<6;i++){
-                        newArr[i] = arr[i]+ arr[ (i+1)%6 ] + arr[(i+3)%6] + arr[ i - 1 == -1 ? 5 : i-1];
-                    }
-                    for(int i=0;i<6;i++){
-                        arr[i] = newArr[i];
-                    }
-
-                }else{
-                    break;
-                }
+            while(sum<=n){
+                count++;
+                sum*=4;
             }
 
             sb.append(count+"\n");
