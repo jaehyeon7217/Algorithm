55b7a0150d1bd4dc544fd6b84cb916f0b35a6f93
Park JaeHyeon <jhp1276@naver.com>
2025-02-17 21:05:04 +0900

    [Bronze II] Title: 대표값2, Time: 68 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[5];
+
+        int sum = 0;
+        for(int i=0;i<5;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            sum+= arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println((int)(sum/5));
+        System.out.println(arr[2]);
+
+
+    }
+
+}
