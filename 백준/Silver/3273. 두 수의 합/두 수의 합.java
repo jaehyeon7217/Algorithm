791ff5e78791b26c3d7ed0f07277bb1f011c9174
Park JaeHyeon <jhp1276@naver.com>
2025-02-18 13:07:11 +0900

    [Silver III] Title: 두 수의 합, Time: 188 ms, Memory: 26364 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -10,27 +10,28 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         int[] arr = new int[n];
-
+        boolean[] check = new boolean[1_000_001];
 
         st = new StringTokenizer(br.readLine(), " ");
         for(int i=0;i<n;i++){
             arr[i] = Integer.parseInt(st.nextToken());
+            check[arr[i]] = true;
         }
 
         int x = Integer.parseInt(br.readLine());
 
-        HashSet<Integer> hash = new HashSet<>();
 
         int count = 0;
 
         for(int i=0;i<n;i++){
-            if(hash.contains(x-arr[i])){
+            int temp = x-arr[i];
+            if(temp<0 || temp>1000000) continue;
+            if(check[temp]){
                 count++;
             }
-            hash.add(arr[i]);
         }
 
-        System.out.println(count);
+        System.out.println(count/2);
 
 
 
