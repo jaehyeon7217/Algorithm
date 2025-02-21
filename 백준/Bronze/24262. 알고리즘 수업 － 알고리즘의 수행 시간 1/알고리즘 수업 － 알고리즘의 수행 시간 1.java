96fe71ec214814cdf995304eff8315c78cfdb753
Park JaeHyeon <jhp1276@naver.com>
2025-02-21 20:46:05 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 68 ms, Memory: 11400 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("1\n0");
+        System.out.println(sb);
+
+    }
+
+
+}
