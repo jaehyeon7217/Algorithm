69304216273f2f0d0a008ac6fe802a4e6963ee04
Park JaeHyeon <jhp1276@naver.com>
2025-02-24 01:27:50 +0900

    [Silver I] Title: 나이트의 이동, Time: 376 ms, Memory: 74740 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static int[] dx = {-1, -2, -2, -1, 1, 2, 2, 1};
+    public static int[] dy = {-2, -1, 1, 2, -2, -1, 1, 2};
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        while(T-- > 0){
+            int n = Integer.parseInt(br.readLine());
+
+            int[][] map = new int[n][n];
+            boolean[][] check = new boolean[n][n];
+
+            st = new StringTokenizer(br.readLine(), " ");
+
+            node start = new node(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+
+            st = new StringTokenizer(br.readLine(), " ");
+
+            node end = new node(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+
+            Queue<node> q = new LinkedList<>();
+            q.offer(start);
+            check[start.x][start.y] = true;
+
+            while(!q.isEmpty()){
+                node temp = q.poll();
+
+                for(int i=0;i<8;i++){
+                    int x = temp.x + dx[i];
+                    int y = temp.y + dy[i];
+
+                    if(x<0 || x>=n || y<0 || y>=n) continue;
+                    if(check[x][y]) continue;
+
+                    check[x][y] = true;
+
+                    map[x][y] = map[temp.x][temp.y] + 1;
+
+                    if(x == end.x && y==end.y){
+                        q.clear();
+                        break;
+                    }
+
+                    q.offer(new node(x,y));
+                }
+            }
+
+            sb.append(map[end.x][end.y] + "\n");
+
+        }
+
+        System.out.println(sb);
+
+
+    }
+
+    public static class node{
+        int x;
+        int y;
+
+        public node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
+
+}
