b6e2aa2c68d9c12b4ab316ff3f010827a9f751df
Park JaeHyeon <jhp1276@naver.com>
2025-02-27 18:41:10 +0900

    [Bronze V] Title: A+B, Time: 68 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,14 +1,19 @@
 import java.io.*;
 import java.util.*;
-public class Main{
-public static void main(String[] args) throws Exception{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    
-    int 에이 = Integer.parseInt(st.nextToken());
-    int 비 = Integer.parseInt(st.nextToken());
-    
-    System.out.println(에이 + 비);
-    
-}
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        
+        st.nextToken();
+        int a = (int)st.nval;
+        st.nextToken();
+        int b = (int)st.nval;
+
+        System.out.println(a+b);
+    }
+
+
 }
