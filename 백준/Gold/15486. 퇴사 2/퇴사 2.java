48ee6d04facf9a502b3e43494362b84401c800e8
Park JaeHyeon <jhp1276@naver.com>
2025-02-27 17:22:24 +0900

    [Gold V] Title: 퇴사 2, Time: 324 ms, Memory: 18964 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -5,19 +5,20 @@ public class Main {
 
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StreamTokenizer st = new StreamTokenizer(System.in);
 
-        int n = Integer.parseInt(br.readLine());
+        st.nextToken();
+        int n = (int)st.nval;
 
         int[] dp = new int[n + 1];
 
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int t = Integer.parseInt(st.nextToken());
-            int p = Integer.parseInt(st.nextToken());
+            st.nextToken();
+            int t = (int)st.nval;
+            st.nextToken();
+            int p = (int)st.nval;
 
-            dp[i+1] = Math.max(dp[i + 1], dp[i]);
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
 
             if (i + t <= n) {
                 dp[i + t] = Math.max(dp[i + t], dp[i] + p);
