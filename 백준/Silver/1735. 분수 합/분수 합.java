6b6cf12c5d7e70f0f8371578ac7705d31b2035d9
Park JaeHyeon <jhp1276@naver.com>
2025-03-10 18:11:32 +0900

    [Silver III] Title: 분수 합, Time: 1068 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.java
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -10,38 +10,6 @@ public class Main {
         int c = nextInt();
         int d = nextInt();
 
-        // 두 분수가 기약분수인지 확인
-
-        while(true) {
-            int temp = Math.min(a, b);
-            boolean flag = true;
-            for (int i = 2; i <= temp; i++) {
-                if (a % i == 0 && b % i == 0) {
-                    a /= i;
-                    b /= i;
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) break;
-        }
-
-
-        while(true) {
-            int temp = Math.min(c, d);
-            boolean flag = true;
-            for (int i = 2; i <= temp; i++) {
-                if (c % i == 0 && d % i == 0) {
-                    c /= i;
-                    d /= i;
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) break;
-        }
-
-
         int e = (a*d) + (b*c);
         int f = b*d;
 
@@ -61,16 +29,8 @@ public class Main {
 
         System.out.println(e + " " + f);
     }
-
-
-
-
-
-
     public static int nextInt() throws Exception {
         st.nextToken();
         return (int) st.nval;
     }
-
-
 }
