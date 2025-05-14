c3c8d13443b36d9057803b34c4c8651e0a323480
Park JaeHyeon <jhp1276@naver.com>
2025-05-15 01:13:44 +0900

    [level 1] Title: 최댓값 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59415. 최댓값 구하기/최댓값 구하기.sql
+++ 프로그래머스/1/59415. 최댓값 구하기/최댓값 구하기.sql
@@ -1,8 +1,5 @@
 -- 코드를 입력하세요
 -- 가장 최근에 들어온 동물은
 -- 언제 들어왔는지 조회
-SELECT DATETIME
-from ANIMAL_INS 
-where DATETIME = (select MAX(DATETIME)
-                  from ANIMAL_INS)
-                  
+select MAX(DATETIME)
+from ANIMAL_INS
